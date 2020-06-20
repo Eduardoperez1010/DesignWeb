bb2a0e5e70de29d4cc4c789662ac4ac3e1d7be70
Eduardo Perez <lalo383596@gmail.com>
2020-06-20 18:22:47 -0500

    Modificando el js

--- javascript.js
+++ javascript.js
@@ -9,10 +9,11 @@ $(document).ready(function(){
 				var urlPath = window.location.pathname;				
 				$('nav a').each(function(){					
 					var href = $(this).attr('href');
+					console.log(href);					
 					var indice =urlPath.length - href.length;		
 					if(urlPath.substring(indice) === href){						
 						$(this).closest('li').addClass('active');	
-
+						console.log('match');
 					}
 				})
 			}
