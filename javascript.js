e146c7fa268e7177ca79dd3684f94b51282d7ac0
Eduardo Perez <lalo383596@gmail.com>
2020-06-20 18:01:55 -0500

    Agregando funcionalidades js

---
+++ javascript.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+			// ponemos el codigoo javascript que utiliza JQuery		
+			// Configurar el inicio
+			configurarInicio();
+			$('.myicon').click(function(e){
+				$('#navbar ul').toggleClass('list_sm');
+			})
+			function configurarInicio(){
+				var urlPath = window.location.pathname;				
+				$('nav a').each(function(){					
+					var href = $(this).attr('href');
+					var indice =urlPath.length - href.length;		
+					if(urlPath.substring(indice) === href){						
+						$(this).closest('li').addClass('active');	
+
+					}
+				})
+			}
+		})			
